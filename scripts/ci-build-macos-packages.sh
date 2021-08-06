1016be7a5d0d978fdffae8eab8aef5a924348658
Steve Kirkland <steve@bugsnag.com>
2021-08-06 21:26:33 +0100

    TEMP COMMIT - Keep on running!

--- scripts/ci-build-macos-packages.sh
+++ scripts/ci-build-macos-packages.sh
@@ -8,14 +8,6 @@ if [[ $CODE != 0 ]]; then
   exit $CODE
 fi
 
-./features/scripts/build_maze_runner.sh webgl
-CODE=$?
-if [[ $? != 0 ]]; then
-  echo "Error, exit code: $CODE"
-  exit $CODE
-fi
-
 pushd features/fixtures/maze_runner/build
   zip -r MacOS-$UNITY_VERSION.zip MacOS
-  zip -r WebGL-$UNITY_VERSION.zip WebGL
 popd
