809481dfa44a9e23a3d061036752e1dcb3ad28ab
Steve Kirkland <steve@bugsnag.com>
2021-08-06 21:23:31 +0100

    Ensure app is not marked as damaged

--- scripts/ci-run-macos-tests.sh
+++ scripts/ci-run-macos-tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 pushd features/fixtures/maze_runner/build
   unzip MacOS-$UNITY_VERSION.zip
+  # Ensure app is not marked as damaged
+  xattr -cr MacOS/Mazerunner.app
 popd
-
+sleep 1
 bundle install
 bundle exec maze-runner --app=features/fixtures/maze_runner/build/MacOS/Mazerunner.app --os=macos features/desktop
