af2b7d79809df1763aae52cb63eaa89f9f28ddfb
Steve Kirkland <steve@bugsnag.com>
2021-08-06 22:18:31 +0100

    Try timing change

--- scripts/ci-run-macos-tests.sh
+++ scripts/ci-run-macos-tests.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 pushd features/fixtures/maze_runner/build
+  rm -rf MacOS
   unzip MacOS-$UNITY_VERSION.zip
   # Ensure app is not marked as damaged
   xattr -cr MacOS/Mazerunner.app
 popd
-sleep 1
 bundle install
+sleep 3
 bundle exec maze-runner --app=features/fixtures/maze_runner/build/MacOS/Mazerunner.app --os=macos features/desktop
