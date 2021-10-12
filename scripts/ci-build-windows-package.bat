ae3cfd78d1d77bdf2b0acfad19ba33fd3e867e29
Steve Kirkland-Walton <steve@bugsnag.com>
2021-10-12 17:18:20 +0100

    Cut down

--- scripts/ci-build-windows-package.bat
+++ scripts/ci-build-windows-package.bat
@@ -1,5 +1,4 @@
 REM Using the artifacts plugin v1.3 on Windows seems to break the whole step
-buildkite-agent artifact download "Bugsnag.unitypackage" .
 cd features\scripts
 C:\Progra~1\Git\bin\bash.exe build_maze_runner.sh windows
 cd ..\fixtures\maze_runner\build
